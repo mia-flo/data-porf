049a98840433f8497324fc3867eeaf296d04e927
Mia Flores <floresdmia@gmail.com>
2026-05-06 11:51:45 -0700

    updated visuals and content. added first gemini project.

--- js/script.js
+++ js/script.js
@@ -1,98 +1,80 @@
 // hamburger menu for tablet/mobile
-const hamMenu = document.querySelector('.ham-menu');
+const hamMenu = document.querySelector(".ham-menu");
 
-const offScreenMenu = document.querySelector('.off-screen-menu');
+const offScreenMenu = document.querySelector(".off-screen-menu");
 
-hamMenu.addEventListener('click', () =>
-{
-    hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
-})
-
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
 
 // scroll back to top
 
-window.onscroll = function () { scrollFunction() };
+window.onscroll = function () {
+  scrollFunction();
+};
 
-function scrollFunction()
-{
-    let topbutton = document.getElementsByClassName("to-top");
-    if (document.body.scrollTop > 20 || documentElement.scrollTop > 20)
-    {
-        topbutton.classList.add("show");
-        console.log("show")
-    } else
-    {
-        topbutton.classList.remove("show");
-        console.log("don't show")
-    }
+function scrollFunction() {
+  let topbutton = document.getElementsByClassName("to-top");
+  if (document.body.scrollTop > 20 || documentElement.scrollTop > 20) {
+    topbutton.classList.add("show");
+    console.log("show");
+  } else {
+    topbutton.classList.remove("show");
+    console.log("don't show");
+  }
 }
 
-function scrollToTop()
-{
-    document.body.scrollTop = 0; //Safari
-    document.documentElement.scrollTop = 0; //Chrome, Firefox, Internet Explorer
+function scrollToTop() {
+  document.body.scrollTop = 0; //Safari
+  document.documentElement.scrollTop = 0; //Chrome, Firefox, Internet Explorer
 }
-
-
 
 // filtering projects based on tags
 
 filterElements("all");
 
-function filterElements(selected)
-{
-    var tags, i;
-    tags = document.getElementsByClassName("project-card-filt")
-    if (selected == "all") selected = "";
+function filterElements(selected) {
+  var cards, i;
+  cards = document.getElementsByClassName("project-card-filt");
+  filters = document.getElementsByClassName("filter");
 
-    for (i = 0; i < tags.length; i++)
-    {
-        removeFilter(tags[i], "show");
-        if (tags[i].className.indexOf(selected) > -1) addFilter(tags[i], "show")
-    }
+  // default selection of 'all'
+  if (selected == "all") {
+    selected = "";
+    filters[0].classList.add("selected");
+  } else {
+    filters[0].classList.remove("selected");
+  }
+
+  for (i = 0; i < cards.length; i++) {
+    removeFilter(cards[i], "show");
+    if (cards[i].className.indexOf(selected) > -1) addFilter(cards[i], "show");
+  }
 }
 
-// adding a filter
-function addFilter(element, name)
-{
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++)
-    {
-        if (arr1.indexOf(arr2[i]) == -1)
-        {
-            element.className += " " + arr2[i];
-        }
+// adding a filter - adds 'show' class to the cards
+function addFilter(tag, name) {
+  var i, tagClasses, newClasses;
+  tagClasses = tag.className.split(" ");
+  newClasses = name.split(" ");
+  for (i = 0; i < newClasses.length; i++) {
+    if (tagClasses.indexOf(newClasses[i]) == -1) {
+      tag.className += " " + newClasses[i];
     }
+  }
 }
 
-
-function removeFilter(element, name)
-{
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++)
-    {
-        while (arr1.indexOf(arr2[i]) > -1)
-        {
-            arr1.splice(arr1.indexOf(arr2[i]), 1);
-        }
+// removing a filter
+function removeFilter(element, name) {
+  var i, cardClasses, classesRemoved;
+  cardClasses = element.className.split(" ");
+  classesRemoved = name.split(" ");
+  for (i = 0; i < classesRemoved.length; i++) {
+    while (cardClasses.indexOf(classesRemoved[i]) > -1) {
+      cardClasses.splice(cardClasses.indexOf(classesRemoved[i]), 1);
     }
-    element; element.className = arr1.join(" ");
+  }
+  element;
+  element.className = cardClasses.join(" ");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ src/script.js
@@ -0,0 +1,77 @@
+const clientId = "adaf64fcd5f540a7bb5b0d28483de761"
+const code = undefined;
+
+
+if (!code) {
+    redirectToAuthCodeFlow(clientId);
+} else {
+    const accessToken = await getAccessToken(clientId, code);
+    const profile = await fetchProfile(accessToken);
+    populateUI(profile);
+}
+
+async function redirectToAuthCodeFlow(clientId: string) {
+    // To do: redirect to spotify authorization page
+    
+}
+
+async function getAccessToken(clientId: String, code: string) {
+    // to do: get access token for code
+}
+
+async function fetchProfile(token: string): Promise<any> {
+    // to do: call web API
+}
+
+function populateUI(profile: any) {
+    // to do: update UI with profile data
+}
+
+
+
+
+export async function redirectToAuthCodeFlow(clientId: string) {
+    const verifier = generateCodeVerifier(128);
+    const challenge = await generateCodeChallenge(verifier);
+
+    localStorage.setItem("verifier", verifier);
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("response_type", "code");
+    params.append("redirect_uri", "http://127.0.0.1:3000/callback");
+    params.append("scope", "user-read-private user-read-email");
+    params.append("code_challenge_method", "S256");
+    params.append("code_challenge", challenge);
+
+    document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+}
+
+function generateCodeVerifier(length: number) {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+async function generateCodeChallenge(codeVerifier: string) {
+    const data = new TextEncoder().encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, ''); 
+}
+
+
+
+
+
+
+
+
+
+
